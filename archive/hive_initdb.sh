2d63b143c3297c9d9653982e099b540867f40560
John G <gentijo@gmail.com>
2021-10-27 17:44:22 -0700

    rm archive

--- archive/hive_initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -f "/opt/hive/bin/hive_initidb.done" ]; then
-  touch /opt/hive/bin/hive_initidb.done
- /opt/hive/bin/schematool -dbType postgres -initSchema
-fi
-
-/opt/hive/bin/schematool -dbType postgres -info
