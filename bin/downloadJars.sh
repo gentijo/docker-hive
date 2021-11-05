6d5238f8350fc4dc87dd0a50a54c0dab1515716c
John Gentilin <gentijo@gmail.com>
2021-11-04 22:44:07 -0700

    Ad;ded fixes for download jars and configured webhdfs wq

--- bin/downloadJars.sh
+++ bin/downloadJars.sh
@@ -5,4 +5,4 @@ export HIVE_VERSION=3.1.2
 wget https://archive.apache.org/dist/hive/hive-$HIVE_VERSION/apache-hive-$HIVE_VERSION-bin.tar.gz
 
 export ZOOKEEPER_VERSION=3.7.0
-https://dlcdn.apache.org/zookeeper/zookeeper-${ZOOKEEPER_VERSION}/apache-zookeeper-${ZOOKEEPER_VERSION}-bin.tar.gz
+wget https://dlcdn.apache.org/zookeeper/zookeeper-${ZOOKEEPER_VERSION}/apache-zookeeper-${ZOOKEEPER_VERSION}-bin.tar.gz
